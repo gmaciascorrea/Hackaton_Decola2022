82dcf3a71b1273482cb205a3f50fd249afb3fdf5
Philipe Serafim <philipeserafim@gmail.com>
2022-03-19 15:33:18 -0300

    Adicionado cardItem à homeview

---
+++ src/components/CardItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="80vw">
+      <v-img :src="url" height="220px"></v-img>
+      <v-card-title>
+        {{ nome }}
+      </v-card-title>
+
+      <v-card-subtitle>
+        <div class="text-left">
+          <v-card-text>
+            <p><strong>Preço:</strong>{{ preco }}</p>
+            <p><strong>Sabor:</strong>{{ sabor }}</p>
+            <p><strong>Nome:</strong>{{ local.nome }}</p>
+            <p><strong>Local:</strong>{{ local.endereco }}</p>
+            <p><strong>CEP:</strong>{{ local.cep }}</p>
+          </v-card-text>
+        </div>
+      </v-card-subtitle>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { integer } from "vee-validate/dist/rules";
+export default {
+  name: "CardItem",
+  props: {
+    id: integer,
+    url: String,
+    local: {
+      cep: String,
+      endereco: String,
+      id: integer,
+      nome: String,
+    },
+    nome: String,
+    preco: String,
+    sabor: String,
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,36 @@
 <template>
   <v-conteiner>
     <h2>Todos os Ovos</h2>
-    <div v-for="ovo of ovos" :key="ovo.id">
-      <v-card class="mx-auto" max-width="80vw">
-    <v-img :src="ovo.imagem" :alt="ovo.id"
-        height="50 vh"
-    ></v-img>
-
-    <v-card-title>
-      {{ ovo.nome }}
-    </v-card-title>
-
-    <v-card-subtitle>
-        <div class="text-left">
-             <v-card-text>
-               <p><strong>Preço:</strong>{{ ovo.preco }}</p>
-               <p><strong>Sabor:</strong>{{ ovo.sabor }}</p>
-               <p><strong>Local:</strong>{{ ovo.local.endereco }}</p>
-        </v-card-text>
-
-        </div>
-      
-    </v-card-subtitle>
-
-  </v-card>
-
-    </div>
+    <CardItem
+      v-for="item in ovos" :key="item.id"
+    :nome="item.nome"
+    :id="item.id"
+    :url="item.imagem"
+    :preco="item.preco"
+    :sabor="item.sabor"
+    :local="item.local"
+    />
   </v-conteiner>
 </template>
 
 <script>
-  //import CardItem from '../components/shared/CardItem.vue'
-  export default {    
-    name: 'HomeView',
-    components: {
-      //CardItem
-    },
-    created() {
-      fetch("https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json")
-      .then(retorno => retorno.json())
-      .then(json => {
+import CardItem from '../components/CardItem.vue'
+export default {
+  name: "HomeView",
+  components: {
+    CardItem
+  },
+  created() {
+    fetch("https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json")
+      .then((retorno) => retorno.json())
+      .then((json) => {
         this.ovos = json;
-        console.log(this.ovos)
-      })
-    },
-    data() {
-      return {
-        ovos: []
-      }
-    }
-
-  }
+      });
+  },
+  data() {
+    return {
+      ovos: [],
+    };
+  },
+};
 </script>
